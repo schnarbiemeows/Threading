2c3e969c0175677a9e5a8cc48cd05193328874f6
u188242 <u188242@L3F0Y3M2.bcbsnc.com>
2018-10-04 17:57:37 -0400

    initial commit

---
+++ Threads3/src/App.java
@@ -0,0 +1,46 @@
+
+public class App {
+
+	private int count = 0;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		App app = new App();
+		app.doWork();
+	}
+
+	private void doWork() {
+		// TODO Auto-generated method stub
+		Thread t1 = new Thread(new Runnable() {
+			public void run() {
+				
+				for(int i=0; i<10000; i++) {
+					count++;
+				}
+			}
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+			public void run() {
+				
+				for(int i=0; i<10000; i++) {
+					count++;
+				}
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Count is: " + count);
+	}
+
+}
