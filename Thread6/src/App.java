9e92238d93c65dcfe45b5b0b00ea9b5ad9b70535
dylan kessler <dylnksslr53@gmail.com>
2018-10-11 16:34:20 -0400

    commiting Threads7

--- Thread6/src/App.java
+++ Thread6/src/App.java
@@ -1,8 +1,48 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+class Processor implements Runnable {
+	
+	private CountDownLatch latch;
+	
+	public Processor(CountDownLatch latch) {
+		this.latch = latch;
+	}
+	public void run() {
+		System.out.println("Started.");
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		latch.countDown();
+	}
+}
 public class App {
 
 	public static void main(String[] args) {
 		// tutorial 6: countdown latches
+		
+		// CountDownLatch is thread safe: no need for synchronized keyword
+		CountDownLatch latch = new CountDownLatch(3);
+		
+		ExecutorService executor = Executors.newFixedThreadPool(3);
+		
+		for(int i=0; i<3; i++) {
+			executor.submit(new Processor(latch));
+		}
+		
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Completed.");
 	}
 
 }
