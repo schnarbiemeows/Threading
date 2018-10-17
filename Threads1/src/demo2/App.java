a3248e338d9cd1ec2ccd9554168d2ba902158ef0
u188242 <u188242@L3F0Y3M2.bcbsnc.com>
2018-10-17 18:59:09 -0400

    commit heaps

--- Threads1/src/demo2/App.java
+++ Threads1/src/demo2/App.java
@@ -8,7 +8,7 @@ class Runner implements Runnable {
 			System.out.println("Hello " + i);
 			
 			try {
-				Thread.sleep(100);
+				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
