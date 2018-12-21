30e942705bb6c8dd64112802367672d20de3139a
dylan kessler <dylnksslr53@gmail.com>
2018-12-20 22:48:17 -0500

    committing countToMillion example

--- Threads1/src/main/MillionThread.java
+++ Threads1/src/main/MillionThread.java
@@ -2,23 +2,22 @@ package main;
 
 public class MillionThread implements Runnable {
 
-	private int count = 0;
+	private static int count = 0;
 	private Object lock = new Object();
 	
 	public void increment() {
 		synchronized(lock) {
+			if(count<1000000)
 			count++;
 		}
 	}
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		while(count<1000000) {
-			increment();
-		}
-		if(count==1000000) {
-			System.out.println("hit");
-		}
+		
+		increment();
+		
+		System.out.println("hit");
 	}
 
 }
